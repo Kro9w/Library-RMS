f044caa66766a7ce687140459860a8fc39ca5d45
Jake Calantas <jakecalantas.blis@gmail.com>
2026-04-08 08:17:29 +0800

    Refactored logging to be transactional

--- apps/api/src/log/log.service.ts
+++ apps/api/src/log/log.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class LogService {
@@ -10,13 +11,37 @@ export class LogService {
     institutionId: string,
     action: string,
     roles: string[],
-    targetName?: string, // Optional parameter for enriched logging
+    targetName?: string,
     campusId?: string,
     departmentId?: string,
   ) {
     const actionString = targetName ? `${action}: '${targetName}'` : action;
 
     await this.prisma.log.create({
+      data: {
+        action: actionString,
+        userId,
+        institutionId,
+        campusId,
+        departmentId,
+        userRole: roles.join(', '),
+      },
+    });
+  }
+
+  async logActionTx(
+    tx: Prisma.TransactionClient,
+    userId: string,
+    institutionId: string,
+    action: string,
+    roles: string[],
+    targetName?: string,
+    campusId?: string,
+    departmentId?: string,
+  ) {
+    const actionString = targetName ? `${action}: '${targetName}'` : action;
+
+    await tx.log.create({
       data: {
         action: actionString,
         userId,
@@ -55,6 +80,34 @@ export class LogService {
     }));
 
     await this.prisma.log.createMany({
+      data,
+    });
+  }
+
+  async logActionsTx(
+    tx: Prisma.TransactionClient,
+    logs: {
+      userId: string;
+      institutionId: string;
+      action: string;
+      roles: string[];
+      targetName?: string;
+      campusId?: string;
+      departmentId?: string;
+    }[],
+  ) {
+    const data = logs.map((log) => ({
+      action: log.targetName
+        ? `${log.action}: '${log.targetName}'`
+        : log.action,
+      userId: log.userId,
+      institutionId: log.institutionId,
+      campusId: log.campusId,
+      departmentId: log.departmentId,
+      userRole: log.roles.join(', '),
+    }));
+
+    await tx.log.createMany({
       data,
     });
   }
